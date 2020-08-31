a61038e8537656e1f22462fb126ce1cead189259
Mateus Gomes <mtsgomesp@gmail.com>
2020-08-31 18:54:34 -0300

    User repository & User model

---
+++ backend/src/main/java/com/eventapp/backend/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eventapp.backend.Repository;
+
+import com.eventapp.backend.Model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByName(String name);
+}
