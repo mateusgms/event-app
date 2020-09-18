891d41828539262ce176979265f7ebf9ba66c56f
Mateus Gomes <mtsgomesp@gmail.com>
2020-09-18 20:09:57 -0300

    fixing route params

--- backend/src/main/java/com/eventapp/backend/Controller/VoucherController.java
+++ backend/src/main/java/com/eventapp/backend/Controller/VoucherController.java
@@ -2,7 +2,11 @@ package com.eventapp.backend.Controller;
 
 import java.util.List;
 
+import com.eventapp.backend.Model.Event;
+import com.eventapp.backend.Model.User;
 import com.eventapp.backend.Model.Voucher;
+import com.eventapp.backend.Repository.EventRepository;
+import com.eventapp.backend.Repository.UserRepository;
 import com.eventapp.backend.Services.VoucherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,33 +26,48 @@ public class VoucherController {
     
 
     @Autowired
-    private VoucherService service;
+    private VoucherService voucherService;
+    
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private EventRepository eventRepository;
+    
     //CREATE
     @PostMapping(value = "/vouchers")
-    public Voucher addVoucher(@RequestBody Voucher voucher) {
-
-        return service.saveVoucher(voucher);
+    public Voucher addVoucher(@RequestBody Voucher voucher,@RequestParam("userId") int userId, @RequestParam("eventId") int eventId) {
+        try {
+            User user = userRepository.findById(userId).get();
+            Event event = eventRepository.findById(eventId).get();
+            voucher.setUser(user);
+            voucher.setEvent(event);
+            Voucher voucherSave = voucherService.saveVoucher(voucher);
+            return voucherSave;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+        
         
     }
     // GET
     @GetMapping("/vouchers")
     public List<Voucher> findAllVouchers() {
-        return service.getVouchers();
+        return voucherService.getVouchers();
     }
 
     @GetMapping(value="/vouchers/id/{id}")
     public Voucher getVoucherById(@RequestParam int id) {
-        return service.getVoucherById(id);
+        return voucherService.getVoucherById(id);
     }
     @PutMapping("/vouchers")
      public Voucher updateVoucher(@RequestBody Voucher voucher) {
-         return service.updateVoucher(voucher);
+         return voucherService.updateVoucher(voucher);
      }
  
      // DELETE
      @DeleteMapping("/vouchers/{id}")
      public String deleteVoucher(@PathVariable int id) {
-         return service.deleteVoucher(id);
+         return voucherService.deleteVoucher(id);
      }
 }
