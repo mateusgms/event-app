aa20b4213d5bac11a17632af493a948242e6c4c9
maxnoturno <marcos_costa_braga@hotmail.com>
2020-10-06 04:45:35 -0300

    Implementado a criação de Token

--- backend/src/main/java/com/eventapp/backend/BackendApplication.java
+++ backend/src/main/java/com/eventapp/backend/BackendApplication.java
@@ -2,16 +2,16 @@ package com.eventapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	@Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	/*
+	 * @Bean public BCryptPasswordEncoder bCryptPasswordEncoder() { return new
+	 * BCryptPasswordEncoder(); }
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
--- backend/src/main/java/com/eventapp/backend/Security/WebSecurity.java
+++ backend/src/main/java/com/eventapp/backend/Security/WebSecurity.java
@@ -1,48 +1,49 @@
-package com.eventapp.backend.Security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import static com.eventapp.backend.Security.SecurityConstants.SIGN_UP_URL;
-
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-    private UserDetailsService userDetailsService;
-
-    private BCryptPasswordEncoder bEncoder;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .anyRequest().authenticated()
-                .and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bEncoder);
-    }
-
-    @Bean
-    CorsConfigurationSource configurationSource() {
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
-}
+/*
+ * package com.eventapp.backend.Security;
+ * 
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.http.HttpMethod; import
+ * org.springframework.security.config.annotation.authentication.builders.
+ * AuthenticationManagerBuilder; import
+ * org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ * import org.springframework.security.config.annotation.web.configuration.
+ * EnableWebSecurity; import
+ * org.springframework.security.config.annotation.web.configuration.
+ * WebSecurityConfigurerAdapter; import
+ * org.springframework.security.config.http.SessionCreationPolicy; import
+ * org.springframework.security.core.userdetails.UserDetailsService; import
+ * org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; import
+ * org.springframework.web.cors.CorsConfiguration; import
+ * org.springframework.web.cors.CorsConfigurationSource; import
+ * org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+ * 
+ * import static com.eventapp.backend.Security.SecurityConstants.SIGN_UP_URL;
+ * 
+ * @EnableWebSecurity public class WebSecurity extends
+ * WebSecurityConfigurerAdapter {
+ * 
+ * private UserDetailsService userDetailsService;
+ * 
+ * private BCryptPasswordEncoder bEncoder;
+ * 
+ * @Override protected void configure(HttpSecurity http) throws Exception {
+ * http.cors().and().csrf().disable().authorizeRequests()
+ * .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+ * .anyRequest().authenticated() .and().addFilter(new
+ * JWTAuthenticationFilter(authenticationManager())) .addFilter(new
+ * JWTAuthenticationFilter(authenticationManager()))
+ * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+ * }
+ * 
+ * @Override public void configure(AuthenticationManagerBuilder auth) throws
+ * Exception {
+ * auth.userDetailsService(userDetailsService).passwordEncoder(bEncoder); }
+ * 
+ * @Bean CorsConfigurationSource configurationSource() {
+ * 
+ * final UrlBasedCorsConfigurationSource source = new
+ * UrlBasedCorsConfigurationSource();
+ * 
+ * source.registerCorsConfiguration("/**", new
+ * CorsConfiguration().applyPermitDefaultValues()); return source; } }
+ */
